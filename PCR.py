25b099673339666cfe63077c9dc0bdd5a944753a
seperez10 <se.perez10@uniandes.edu.co>
2018-10-30 18:10:03 -0500

    Borramos la primera columna

--- PCR.py
+++ PCR.py
@@ -7,3 +7,6 @@ nd=31.0
 cl= np.arange(0,nd+1)
 #Ordenamos los datos de WDBC.dat en el dataframe para poder leerlos, utilizaremos pandas en esto ya que nos ayuda a evitar los datos que no sean numeros (nan)
 data= pd.read_csv('WDBC.dat',names=cl)
+#Borraremos la primera columna de los datos para facilitar el trabajo del archivo
+data.drop(data.columns[[0]], axis=1, inplace=True)
+
