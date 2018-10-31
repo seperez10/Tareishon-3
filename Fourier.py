087b378dbb422a34621c05725631ff82b69cc1d7
seperez10 <se.perez10@uniandes.edu.co>
2018-10-31 14:54:23 -0500

    Ya grafique :)

--- Fourier.py
+++ Fourier.py
@@ -9,4 +9,14 @@ datos_incompletos= np.genfromtxt('incompletos.dat', usecols=[0,2])
 #####Segundo punto#####
 # Aqui graficaremos los datos delsignal.dat
 t_sg=datos_senal[:,0]
+y_sg=datos_senal[:,1]
+plt.plot(t_sg,y_sg,linestyle=':',label='datos')
+plt.title('datos del signal.dat')
+plt.xlabel(r'$y$')
+plt.ylabel(r'$Y$')
+plt.legend()
+plt.savefig('PerezSantiago_signal.pdf')
+plt.close()
+
+#######Tercer punto#####
 
