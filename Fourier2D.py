62143645bd9eb77357b6a9804329166a8fd90115
seperez10 <se.perez10@uniandes.edu.co>
2018-10-31 17:42:10 -0500

    Aqui empece el ultimo punto wuuuu

---
+++ Fourier2D.py
@@ -0,0 +1,8 @@
+from scipy import fftpack
+import numpy as np
+import matplotlib.pyplot as plt
+
+#####Primer punto######
+####Almacenamos la imagen en un arreglo de numpy 
+img = plt.imread('arbol.png')
+
